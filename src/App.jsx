5f4d3b00a715b1d6ccd672b709b8697ad0718252
Locng-fpt <locnxse183782@fpt.edu.vn>
2026-06-07 13:40:45 +0700

    ban chinh thuc dau tien

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import AviationDashboard from './components/AviationDashboard';
+import ILSMonitorDashboard from './components/Ilsmonitordashboard';
 
 function App() {
   return (
     <div className="App">
-      <AviationDashboard />
+      <ILSMonitorDashboard />
     </div>
   );
 }
