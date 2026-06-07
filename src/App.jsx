614fbe76d1428c34967722022c08949ae028fe54
Locng-fpt <locnxse183782@fpt.edu.vn>
2026-06-07 11:27:26 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AviationDashboard from './components/AviationDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <AviationDashboard />
+    </div>
+  );
+}
+
+export default App;
